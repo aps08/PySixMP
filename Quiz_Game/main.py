c3386fc634cf50e7d35896c0758280a87ea8d793
aps08 <anoopprsingh.social@gmail.com>
2022-03-14 22:57:21 +0530

    check

---
+++ Quiz_Game/main.py
@@ -0,0 +1,13 @@
+import imp
+import requests
+from tkinter import *
+
+class Quiz_Game:
+    def __init__(self) -> None:
+        pass
+    def start(self) -> None:
+        pass
+    
+if __name__ == "__main__":
+    print("Running......")
+    Quiz_Game.start()
