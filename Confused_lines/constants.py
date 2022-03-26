6cd32e951d81d812d595db34bf06f8ed6b4c4225
aps08 <anoopprsingh.social@gmail.com>
2022-03-26 22:53:31 +0530

    Confused Lines Completed

---
+++ Confused_lines/constants.py
@@ -0,0 +1,31 @@
+"""
+This file contains some constants which you could use to customize interface and functionality of this code.
+Before reading further, I suggest you to run the code and see how it works.
+"""
+
+# Title of the screen
+TITEL_TEXT = "Confused Lines! 😉😉😉😉"
+# Background Colour of the screen
+BG_COLOR = "black"
+# Start pen color
+PEN_COLOR = "white"
+# Sleep Time, after how much time the screen should close. In seconds
+SLEEP_TIME = 3
+# Instruction which you see at the top of screen, on start of the program.
+INSTRUCTION_TEXT = "Window will close automatically after it's done"
+# Instruction which you see at the end of program.
+END_INSTRUCTION_TEXT = "Closing window in 3 seconds"
+# This text is shown, when you are prompted to Enter, how many time you want the lines to run. By default it's value is 50. 
+PROMPT_TEXT = "Enter number of times you want to run the lines.\nDefault value:50"
+# Font which you see on the screen.
+FONT = ("Courier", 12, "bold")
+# X co-ordinates for the start of the line. Hortizaontal
+X = 400.0
+# Y co-ordinates for the start of the line. Vertical
+Y = 300.0
+# Angles at which the lines can you. A random value will be select from this list.
+ANGLES = [0, 90, 180, 270]
+# Distance which will be convered for each turn. A random value will selected after each turn.
+FORWARD_BY = [50, 100]
+# Thickness of the line.
+PENSIZE = 10
--- Confused_lines/main.py
+++ Confused_lines/main.py
@@ -1,13 +1,7 @@
-from tkinter import TclError
-from turtle import Terminator, Turtle, Screen, numinput
-import random, time
-
-PROMPT_TEXT = "Enter number of times you want to run the lines.\nDefault value:50"
-FONT = ("Courier", 12, "bold")
-X = 400.0
-Y = 300.0
-ANGLES = [0, 90, 180, 270]
-FORWARD_BY = [30, 50, 70]
+from turtle import Turtle, Screen, numinput
+import random
+import time
+import constants as C
 
 class confused_lines(Turtle):
     """
@@ -18,28 +12,28 @@ class confused_lines(Turtle):
     1. __init__() --> Constructor. Creates the screen and takes the input from user.
     2. write_instructions() --> responsible for showing text on the screen.
     3. run() --> run the loop N times, which is taken from the user, by default the value is 50.
-    4. mirror() --> responsible for mirroring the turtle.
+    4. mirror() --> responsible for mirroring the turtle, on the opposite side of the wall.
     """
 
     def __init__(self) -> None:
         super().__init__()
         self.screen = Screen()
         self.screen.cv._rootwindow.resizable(False, False)
-        self.screen.title("Confused Lines! 😉😉😉😉")
-        self.screen.bgcolor("black")
-        self.screen.setup(width=800, height=600)
+        self.screen.title(C.TITEL_TEXT)
+        self.screen.bgcolor(C.BG_COLOR)
+        self.screen.setup(width=int(C.X)*2, height=int(C.Y)*2)
         self.write_instruction()
-        self.input = int(numinput(title="Number", prompt=PROMPT_TEXT, default=50,minval=50, maxval=9999))
+        self.input = int(numinput(title="Number", prompt=C.PROMPT_TEXT, default=50,minval=50, maxval=9999))
         self.run()
 
     def write_instruction(self) -> None:
         self.hideturtle()
-        self.pensize(10)
+        self.pensize(C.PENSIZE)
         self.penup()
-        self.goto(x=0, y= 270)
+        self.goto(x=0, y= int(C.Y)-40)
         self.pendown()
-        self.pencolor("white")
-        self.write("Window will close automatically after it's done", align="center", font=(FONT))
+        self.pencolor(C.PEN_COLOR)
+        self.write(C.INSTRUCTION_TEXT, align="center", font=(C.FONT))
         self.penup()
         self.home()
         self.pendown()
@@ -48,32 +42,32 @@ class confused_lines(Turtle):
         for i in range(self.input):
             self.mirror()
             self.color(['#%06X' % random.randint(0, 0xFFFFFF) for i in range(1)][0])
-            self.right(random.choice(ANGLES))
-            self.forward(random.choice(FORWARD_BY))
+            self.right(random.choice(C.ANGLES))
+            self.forward(random.choice(C.FORWARD_BY))
         self.clear()
         self.penup()
         self.home()
         self.pendown()
-        self.pencolor("white")
-        self.write("Closing window in 3 seconds", align="center", font=FONT)
-        time.sleep(3)
+        self.pencolor(C.PEN_COLOR)
+        self.write(C.END_INSTRUCTION_TEXT, align="center", font=C.FONT)
+        time.sleep(C.SLEEP_TIME)
 
     def mirror(self) -> None:
-        if self.xcor() < -X:
+        if self.xcor() < -C.X:
             self.penup()
-            self.goto(X, self.ycor())
+            self.goto(C.X, self.ycor())
             self.pendown()
-        if self.xcor() > X:
+        if self.xcor() > C.X:
             self.penup()
-            self.goto(-X, self.ycor())
+            self.goto(-C.X, self.ycor())
             self.pendown()
-        if self.ycor() < -Y:
+        if self.ycor() < -C.Y:
             self.penup()
-            self.goto(self.xcor(), Y)
+            self.goto(self.xcor(), C.Y)
             self.pendown()
-        if self.ycor() > Y:
+        if self.ycor() > C.Y:
             self.penup()
-            self.goto(self.xcor(), -Y)
+            self.goto(self.xcor(), -C.Y)
             self.pendown()
         
 
@@ -82,5 +76,5 @@ if __name__ == "__main__":
     try:
         App = confused_lines()
         print("Closing screen")
-    except Terminator:
+    except:
         print("Forced Stop")
