b3758ff29c60c50932cff6c519207127f81be377
aps08 <anoopprsingh.social@gmail.com>
2022-03-27 19:08:52 +0530

    working

--- Web_Scraping/scraper.py
+++ Web_Scraping/scraper.py
@@ -3,14 +3,22 @@ from bs4 import BeautifulSoup
 import random
 import threading
 
+# Some links for scraping the news.
 CRUNCHHYPE_URL = "https://www.crunchhype.com/"
 WIRED_URL = "https://www.wired.com/feed/google-latest-news/sitemap-google-news"
 NYTIMES_URL = "https://www.nytimes.com/sitemaps/new/news-1.xml.gz"
 THEVERGE_URL = "https://www.theverge.com/sitemaps/google_news"
+# Dictionary variable for storing results.
 NEWS = dict()
 
 
 def news() -> dict:
+    """ This the main function which uses all the below functions to get news data using muti-threading.
+
+    Returns:
+        dict: return the result in dictionary format, where the link of the website is the key.
+                Each key contains a nested list, which contains the title and link of the news.
+    """
     crunchhype_thread = threading.Thread(target=crunchhype_news())
     wired_thread = threading.Thread(target=wired_news())
     nytimes_thread = threading.Thread(target=nytimes_news())
@@ -23,6 +31,8 @@ def news() -> dict:
 
 
 def crunchhype_news() -> None:
+    """ Web scraping news from crunchhype website.
+    """
     Soup = BeautifulSoup(requests.get(CRUNCHHYPE_URL).text, 'lxml')
     links = Soup.find_all(class_="entry-title-link")
     crunchhype_result = [(link["href"], link["title"]) for link in links]
@@ -31,6 +41,8 @@ def crunchhype_news() -> None:
 
 
 def wired_news() -> None:
+    """ Web scraping news from wired website.
+    """
     Soup = BeautifulSoup(requests.get(WIRED_URL).text, 'lxml')
     links = [link.text for link in Soup.findAll("loc")]
     titles = [title.text for title in Soup.findAll("news:title")]
@@ -40,6 +52,8 @@ def wired_news() -> None:
 
 
 def nytimes_news() -> None:
+    """ Web scraping news from nytimes website.
+    """
     Soup = BeautifulSoup(requests.get(NYTIMES_URL).text, 'lxml')
     links = [link.text for link in Soup.findAll("loc")]
     titles = [title.text for title in Soup.findAll("news:title")]
@@ -49,6 +63,8 @@ def nytimes_news() -> None:
 
 
 def theverge_news() -> None:
+    """ Web scraping news from theverge website.
+    """
     Soup = BeautifulSoup(requests.get(THEVERGE_URL).text, 'lxml')
     links = [link.text for link in Soup.findAll("loc")][0:6]
     titles = [title.text for title in Soup.findAll("news:title")]
--- Web_Scraping/server.py
+++ Web_Scraping/server.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """ This API is responsible for showing the first page and populating the page with data from scraper file.
+
+    Returns:
+        return a html template when anyone triggers the root of the server.
+    """
     return render_template("index.html", data=scraper.news())
 
 
